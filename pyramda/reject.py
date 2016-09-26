9d743c851b4c5a0be8fbc79dc33289aa10f83521
David Wickström <davidwickstrom@gmail.com>
2016-09-26 19:32:47 +0200

    PEP8 fixes

--- pyramda/reject.py
+++ pyramda/reject.py
@@ -1,4 +1,6 @@
 from pyramda.function.curry import curry
+from pyramda.iterable.filter import filter
 
 
-reject = curry(lambda predicate, filterable: filter(lambda x: not predicate(x), filterable))
+reject = curry(lambda predicate, filterable: filter(
+    lambda x: not predicate(x), filterable))
--- pyramda/reject_test.py
+++ pyramda/reject_test.py
@@ -1,30 +1,34 @@
 from .reject import reject
 from pyramda.private.asserts import assert_equal
 
-is_even = lambda x: x % 2 == 0
 
-is_odd = lambda x: x % 2 == 1
+def is_even(x):
+    return x % 2 == 0
+
+
+def is_odd(x):
+    return x % 2 == 1
 
 
 def non_curried_reject_test():
     assert_equal(
-        reject(is_even, [0, 1, 2, 3]),
+        list(reject(is_even, [0, 1, 2, 3])),
         [1, 3]
     )
 
     assert_equal(
-        reject(is_odd, [0, 1, 2, 3]),
+        list(reject(is_odd, [0, 1, 2, 3])),
         [0, 2]
     )
 
 
 def curried_reject_test():
     assert_equal(
-        reject(is_even)([0, 1, 2, 3]),
+        list(reject(is_even)([0, 1, 2, 3])),
         [1, 3]
     )
 
     assert_equal(
-        reject(is_odd)([0, 1, 2, 3]),
+        list(reject(is_odd)([0, 1, 2, 3])),
         [0, 2]
     )
